95ee7c7c117b7319493299cfaab1bd5cbc0bf899
Rafael Minguet García <rminguet@gmail.com>
2015-06-20 16:24:38 +0200

    Request for the weather in your location

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -1,5 +1,16 @@
-angular.module('PhonegapMobileangularui.controllers.Main', [])
+angular.module('PhonegapMobileangularui.controllers.Main', [
+    'PhonegapMobileangularui.services.Geolocation',
+    'PhonegapMobileangularui.services.Forecast'
+])
 
-.controller('MainController', function($scope){
-  
+.controller('MainController', function($scope, getCurrentPosition, getWeather){
+    getCurrentPosition(function(position){
+        getWeather(
+            position.coords.latitude,
+            position.coords.longitude,
+            function(location, weather){
+                $scope.location = location;
+                $scope.weather = weather;
+            });
+    });
 });
---
+++ src/js/services/geolocation.js
@@ -0,0 +1,19 @@
+angular.module('PhonegapMobileangularui.services.Geolocation', [
+    'PhonegapMobileangularui.services.Cordova'
+])
+
+.factory('getCurrentPosition', function(deviceReady, $document, $window, $rootScope){
+    return function(done) {
+        deviceReady(function(){
+            navigator.geolocation.getCurrentPosition(function(position){
+                $rootScope.$apply(function(){
+                    done(position);
+                });
+            }, function(error){
+                $rootScope.$apply(function(){
+                    throw new Error('Unable to retreive position');
+                });
+            });
+        });
+    };
+});
